4f4afe607fb17b685aea19eb737eb915579a44c0
lebronejames2323 <ginovillar02@gmail.com>
2025-07-06 20:37:25 +0800

    added Github Action

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Jobs\UpdateOrderStatusJob;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Request;
 
-Route::get('/test-dispatch', function () {
-    dispatch(new UpdateOrderStatusJob());
-    return '✅ UpdateOrderStatusJob dispatched!';
+Route::get('/run-schedule', function () {
+    $token = Request::get('token');
+
+    if ($token !== env('SCHEDULE_TOKEN')) {
+        abort(403, 'Unauthorized');
+    }
+
+    Artisan::call('schedule:run');
+    return '✅ Scheduler triggered at ' . now();
 });
